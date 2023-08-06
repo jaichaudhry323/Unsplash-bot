6eb527c81e4d1248766b4cff6943d7bed21a6970
Jai Chaudhry <42890838+jaichaudhry323@users.noreply.github.com>
2023-08-06 23:46:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,34 +20,38 @@ app = Flask(__name__)
 # global bot
 # bot = telegram.Bot(token='5602023512:AAETtyiKUXT8b-jmXtkk5jkt79xw9YcD0qo')
 
-# @app.route('/HOOK', methods=['POST'])
-# def webhook_handler():
-#     if request.method == "POST":
-#         # retrieve the message in JSON and then transform it to Telegram object
-#         update = telegram.Update.de_json(request.get_json(force=True))
+@app.route('/hook', methods=['GET','POST'])
+def webhook_handler():
+    print("received message 1")
 
-#         chat_id = update.message.chat.id
-
-#         print("chat_id: ", chat_id)
-
-#         # Telegram understands UTF-8, so encode text for unicode compatibility
-#         text = update.message.text.encode('utf-8')
+    if request.method == "POST":
+        print("received message 2")
         
-#         # repeat the same message back (echo)
-#         bot.sendMessage(chat_id=chat_id, text=text)
+        # retrieve the message in JSON and then transform it to Telegram object
+        update = telegram.Update.de_json(request.get_json(force=True))
 
-#     return 'ok'
+        chat_id = update.message.chat.id
+
+        print("chat_id: ", chat_id)
+
+        # Telegram understands UTF-8, so encode text for unicode compatibility
+        text = update.message.text.encode('utf-8')
+        
+        # repeat the same message back (echo)
+        bot.sendMessage(chat_id=chat_id, text=text)
+
+    return 'ok'
 
 
-# @app.route('/set_webhook', methods=['GET', 'POST'])
-# def set_webhook():
-#     s = bot.setWebhook('https://testbot1-jaichaudhry323.b4a.run/HOOK')
-#     if s:
-#         print("webhook setup ok")
-#         return "webhook setup ok"
-#     else:
-#         print("webhook setup not ok")
-#         return "webhook setup failed"
+@app.route('/set_webhook', methods=['GET', 'POST'])
+def set_webhook():
+    s = bot.setWebhook('https://testbot1-jaichaudhry323.b4a.run/HOOK')
+    if s:
+        print("webhook setup ok")
+        return "webhook setup ok"
+    else:
+        print("webhook setup not ok")
+        return "webhook setup failed"
 
     
 @app.route('/healthcheck', methods=['GET'])
