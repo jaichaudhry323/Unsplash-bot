0f7dc6195cb7338e231e4e5a89c13c67fc16da26
Jai Chaudhry <42890838+jaichaudhry323@users.noreply.github.com>
2023-08-06 18:47:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask
-import sys
+# from flask import Flask
+# import sys
 
-app = Flask(__name__)
-# cors = CORS(app)
+# app = Flask(__name__)
+# # cors = CORS(app)
 
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=80)
+# if __name__ == '__main__':
+#   app.run(host='0.0.0.0', port=80)
+
+# import requests
+# resp = requests.get(http://0.0.0.0:80/healthcheck)
+# print("healthcheck response: ", resp)
